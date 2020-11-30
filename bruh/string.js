de18eb790d92be2e7a999c93d3eb2246c6f64c7f
EmmaroseKrusely <70153909+EmmaroseKrusely@users.noreply.github.com>
2020-11-30 11:21:03 -0500

    Add files via upload

---
+++ bruh/string.js
@@ -0,0 +1,15 @@
+// alert("yo");
+    var x = 5;
+    var y = 10;
+    var xSquared = Math.pow(x, 2);
+    var ySquared = Math.pow(y, 2);
+    var z = Math.sqrt(xSquared +ySquared);
+    // var name = "Ninja"
+    console.log("Your name is "+name);
+    console.log("x + y = " + x+y);
+    console.log("x + y = " + parseInt(x+y));
+
+    alert("Hello, what is your name?");
+    name = prompt("Please input your name: ");
+    alert("Welcome, " + name + ". It is great to see you today.");
+    console.log(`Your name is now: ${name} `);
